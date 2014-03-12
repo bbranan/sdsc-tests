5205eed01f04f14746851c489b40eabb043bef21
Bill Branan <bbranan@duraspace.org>
2014-03-12 12:43:33 -0400

    Includes several different attempts to perform content PUTs, using two different clients. Breaks things out so it's more clear what tests are occurring.

--- sdsc-jclouds-1.7.1/SDSCTest.java
+++ sdsc-jclouds-1.7.1/SDSCTest.java
@@ -17,7 +17,12 @@ import org.jclouds.openstack.swift.options.CreateContainerOptions;
 import org.jclouds.openstack.swift.options.ListContainerOptions;
 import org.jclouds.rest.RestContext;
 
-import java.text.SimpleDateFormat;
+import org.jclouds.blobstore.BlobStore;
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.domain.Blob;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -35,15 +40,11 @@ public class SDSCTest {
 
     private static String authUrl =
         "https://duracloud.auth.cloud.sdsc.edu/auth/v1.0";
-    
-    private int plannedAttempts = 5;
+//        "https://auth.api.rackspacecloud.com/v1.0";
 
     // ENTER CREDENTIALS HERE!
     private String username = <REPLACE>;
     private String password = <REPLACE>;
-    
-    private SimpleDateFormat dateFormat = 
-        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
 
     private SwiftClient getFreshSwiftClient() {
         String trimmedAuthUrl = // JClouds expects authURL with no version
@@ -54,9 +55,18 @@ public class SDSCTest {
                              .buildApi(SwiftClient.class);
     }
 
+    private BlobStore getFreshBlobStore() {
+        String trimmedAuthUrl = // JClouds expects authURL with no version
+            authUrl.substring(0, authUrl.lastIndexOf("/"));
+        return ContextBuilder.newBuilder(new SwiftApiMetadata())
+                              .endpoint(trimmedAuthUrl)
+                              .credentials(username, password)
+                              .buildView(BlobStoreContext.class)
+                              .getBlobStore();
+    }
+
     public void testSwiftClient() throws Exception {
         System.out.println("STARTING JCLOUDS 1.7.1 TEST");
-
         SwiftClient swiftClient = getFreshSwiftClient();
 
         System.out.println("TEST: Get Containers");
@@ -73,23 +83,90 @@ public class SDSCTest {
         CreateContainerOptions createContainerOptions =
             CreateContainerOptions.Builder.withMetadata(properties);
         swiftClient.createContainer(containerName, createContainerOptions);
+                
+        String swiftFile = putContentSwiftFile(containerName);
+        String swiftString = putContentSwiftString(containerName);
+        String blob = putContentBlob(containerName);
         
-        String contentName = "test-content-" + System.currentTimeMillis();
-        System.out.println("TEST: PUT content named " + contentName);
+        deleteContent(containerName, swiftFile, swiftString, blob);
+
+        System.out.println("TEST: DELETE Container named " + containerName);
+        swiftClient.deleteContainerIfEmpty(containerName);
+        System.out.println("  DELETE successful");
+
+        System.out.println("JCLOUDS 1.7.1 TEST COMPLETE");
+    }
+
+    // Attempts to push a file using the Swift client
+    public String putContentSwiftFile(String containerName) throws Exception {   
+        SwiftClient swiftClient = getFreshSwiftClient();
+        String contentName = "test-content-swift-file-" + System.currentTimeMillis();
+        System.out.println("TEST SWIFT: PUT content named " + contentName);
+        
+        SwiftObject swiftObject = swiftClient.newSwiftObject();
+        MutableObjectInfoWithMetadata objectInfoMetadata = swiftObject.getInfo();
+        objectInfoMetadata.setName(contentName);
+        InputStream input = new FileInputStream("c:\\test.txt");
+        swiftObject.setPayload(input);
+        
+        try {
+            String checksum = swiftClient.putObject(containerName, swiftObject);
+            System.out.println("  PUT successful, checksum: " + checksum);
+        } catch(Exception e) {
+            System.out.println("  PUT failed with error: " + e.getMessage());
+            //e.printStackTrace();          
+        }
+        return contentName;
+    }
+    
+    // Attempts to push a string value using the Swift client
+    public String putContentSwiftString(String containerName) throws Exception {
+        SwiftClient swiftClient = getFreshSwiftClient();
+        String contentName = "test-content-swift-string-" + System.currentTimeMillis();
+        System.out.println("TEST SWIFT: PUT content named " + contentName);
+        
         SwiftObject swiftObject = swiftClient.newSwiftObject();
         MutableObjectInfoWithMetadata objectInfoMetadata = swiftObject.getInfo();
         objectInfoMetadata.setName(contentName);
         swiftObject.setPayload(contentName);
-        String checksum = swiftClient.putObject(containerName, swiftObject);
-        System.out.println("PUT successful, checksum: " + checksum);
         
-        System.out.println("TEST: DELETE content named " + contentName);
-        swiftClient.removeObject(containerName, contentName);        
-
-        System.out.println("TEST: Delete Container named " + containerName);
-        swiftClient.deleteContainerIfEmpty(containerName);
-
-        System.out.println("JCLOUDS 1.7.1 TEST COMPLETE");
+        try {
+            String checksum = swiftClient.putObject(containerName, swiftObject);
+            System.out.println("  PUT successful, checksum: " + checksum);
+        } catch(Exception e) {
+            System.out.println("  PUT failed with error: " + e.getMessage());
+            //e.printStackTrace();
+        }
+        return contentName;
+    }
+    
+    // Attempts to push a file using the BlobStore client
+    public String putContentBlob(String containerName) throws Exception {
+        BlobStore blobStore = getFreshBlobStore();
+        String contentName = "test-content-blob-" + System.currentTimeMillis();
+        System.out.println("TEST BLOB: PUT content named " + contentName);
+        
+        InputStream input = new FileInputStream("c:\\test.txt");
+        Blob blobs = blobStore.blobBuilder(contentName).payload(input).build();
+        
+        try {
+            String checksum = blobStore.putBlob(containerName, blobs);        
+            System.out.println("  PUT successful, checksum: " + checksum);
+        } catch(Exception e) {
+            System.out.println("  PUT failed with error: " + e.getMessage());
+            //e.printStackTrace();
+        }
+        return contentName;
+    }
+    
+    // Attempts to delete content
+    public void deleteContent(String containerName, String... contentNames) {
+        SwiftClient swiftClient = getFreshSwiftClient();
+        for(String contentName : contentNames) {
+            System.out.println("TEST: DELETE content named " + contentName);
+            swiftClient.removeObject(containerName, contentName);
+            System.out.println("  DELETE successful");
+        }
     }
 
     // Start the ball rolling    
